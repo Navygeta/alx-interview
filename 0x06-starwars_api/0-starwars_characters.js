465d0d303dab405971cb4ef3e1564f1774e0629f
Navygeta <tinashenavonika@gmail.com>
2024-06-07 00:33:52 +0200

    star wARS CHARS 2

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,24 +4,34 @@ const request = require('request');
 const filmId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
 
-const fetchCharacterNames = (characters) => {
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (err, response, body) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      const character = JSON.parse(body);
-      console.log(character.name);
-    });
-  });
-};
-
 request(url, (err, response, body) => {
   if (err) {
     console.error(err);
     return;
   }
   const film = JSON.parse(body);
-  fetchCharacterNames(film.characters);
+  const characters = film.characters;
+  
+  const printCharacterName = (characterUrl, callback) => {
+    request(characterUrl, (err, response, body) => {
+      if (err) {
+        console.error(err);
+        return callback(err);
+      }
+      const character = JSON.parse(body);
+      console.log(character.name);
+      callback();
+    });
+  };
+
+  const printAllCharacters = (index) => {
+    if (index >= characters.length) {
+      return;
+    }
+    printCharacterName(characters[index], () => {
+      printAllCharacters(index + 1);
+    });
+  };
+
+  printAllCharacters(0);
 });
